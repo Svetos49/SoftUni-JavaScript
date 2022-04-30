09c25f6fdf1f231be396ae2f63ff23737f14695e
Svetos49 <75615836+Svetos49@users.noreply.github.com>
2022-04-30 21:10:29 +0300

    Add files via upload

---
+++ Programming Basics with JavaScript/For Loop/Lab/evenPowersOf2.js
@@ -0,0 +1,10 @@
+function evenPowersOf2(input) {
+  let num = Number(input[0]);
+  let n = 1;
+  for(let i = 0; i <= num; i += 2) {
+      console.log(n);
+      n = n * 2 * 2;
+  }
+}
+
+evenPowersOf2(['6'])
