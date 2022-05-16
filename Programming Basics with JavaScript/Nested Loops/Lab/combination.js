647147a252c73df40e5bcc9742b7692fd33b86c8
Svetos49 <75615836+Svetos49@users.noreply.github.com>
2022-05-16 23:09:35 +0300

    Add files via upload

---
+++ Programming Basics with JavaScript/Nested Loops/Lab/combination.js
@@ -0,0 +1,16 @@
+function combination(input) {
+  let n = Number(input[0]);
+  let validComb = 0
+  for(x1 = 0; x1 <= n; x1++) {
+      for(x2 = 0; x2 <= n; x2++) {
+          for(x3 = 0; x3 <= n; x3++) {
+             if(x1 + x2 + x3 == n) {
+                 validComb++
+             }
+          }
+      }
+  }
+  console.log(validComb);
+}
+
+combination(['25']);
