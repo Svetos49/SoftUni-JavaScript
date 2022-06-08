0bb0b0f584069fdec51fc9c492500b651b61eee3
Svetos49 <75615836+Svetos49@users.noreply.github.com>
2022-06-08 22:05:21 +0300

    Add files via upload

---
+++ Programming Fundamentals with JS/Basic Syntax, Conditional Statements and Loops/Lab/numbersFromM_ToN.js
@@ -0,0 +1,15 @@
+function solve(m, n) {
+ for(let i = m; i >=n; i--) {
+     console.log(i);
+ }
+}
+
+// function solve(m, n) {
+//     let i = m;
+//     while(i >= n) {
+//         console.log(i);
+//         i--;
+//     }
+// }
+
+solve(6, 2)
