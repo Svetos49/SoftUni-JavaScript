65a5b28a5c3cadd2b81a6256dc9bb3cd043c63d3
Svetos49 <75615836+Svetos49@users.noreply.github.com>
2022-07-06 18:38:11 +0300

    Add files via upload

---
+++ Programming Fundamentals with JS/Functions/Lab/mathPower.js
@@ -0,0 +1,13 @@
+function solve(num, power) {
+    //console.log(num ** power);
+
+   /* let sum = num;
+    for(let i = 1; i < power; i++) {
+      sum *= num;
+    }
+    console.log(sum); */
+
+    console.log(Math.pow(num, power));
+}
+
+solve(2, 8);
---
+++ Programming Fundamentals with JS/Functions/Lab/orders.js
@@ -0,0 +1,17 @@
+function solve(product, quantity) {
+
+    switch(product) {
+      case 'coffee': result = 1.50 * quantity;
+      break;
+      case 'water': result = 1.00 * quantity;
+      break;
+      case 'coke': result = 1.40 * quantity;
+      break;
+      case 'snacks': result = 2.00 * quantity;
+      break;
+      
+    }
+    console.log(result.toFixed(2));
+}
+
+solve("water", 5);
