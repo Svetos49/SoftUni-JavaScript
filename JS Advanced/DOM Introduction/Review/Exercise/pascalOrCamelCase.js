2b01ab5201c24a8e3cb4eda7572c62cc6c93850f
Svetoslav Slavov <75615836+Svetos49@users.noreply.github.com>
2022-11-27 21:10:08 +0200

    Add files via upload

---
+++ JS Advanced/DOM Introduction/Review/Exercise/pascalOrCamelCase.js
@@ -0,0 +1,21 @@
+function solve() {
+    let text = document.getElementById('text').value;
+    let naming = document.getElementById('naming-convention').value;
+    const resultContainer = document.getElementById('result')
+
+    const splited = text.split(" ");
+    let resultString = '';
+    if(naming == "Pascal Case") {
+        for(let i = 0; i < splited.length; i++) {
+            resultString += splited[i][0].toUpperCase() + splited[i].slice[1].toLowerCase();
+        }
+    } else if(naming == "Camel Case") {
+        resultSplited += splited[0][0] + splited[0].slice[1].toLowerCase;
+        for(let i = 1; i < splited.length; i++) {
+            resultString += splited[i][0].toUpperCase() + splited[i].slice[1].toLowerCase();
+        }
+    } else {
+        resultContainer.textContent = 'Error!';
+    }
+    resultContainer.textContent = resultString;
+}
