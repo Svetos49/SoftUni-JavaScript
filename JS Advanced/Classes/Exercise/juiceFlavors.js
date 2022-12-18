fe1e793e42f61ad8d5b78009b270e09596caf27b
Svetoslav Slavov <75615836+Svetos49@users.noreply.github.com>
2022-12-18 11:11:47 +0200

    Add files via upload

---
+++ JS Advanced/Classes/Exercise/juiceFlavors.js
@@ -0,0 +1,9 @@
+function juiceFlavors(juicesArr) {
+   let juicesAmount = new Map();
+}
+
+juiceFlavors(['Orange => 2000',
+'Peach => 1432',
+'Banana => 450',
+'Peach => 600',
+'Strawberry => 549'])
