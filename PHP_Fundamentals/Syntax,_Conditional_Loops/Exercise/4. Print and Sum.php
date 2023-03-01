4913c2451bc5e71823526a39bdbc981d3b7e76a1
Svetoslav Slavov <75615836+Svetos49@users.noreply.github.com>
2023-03-01 10:08:15 +0200

    Add files via upload

---
+++ PHP_Fundamentals/Syntax,_Conditional_Loops/Exercise/4. Print and Sum.php
@@ -0,0 +1,10 @@
+<?php
+$startNum = intval(readline());
+$endNum = intval(readline());
+$sum = 0;
+for($i = $startNum; $i <=$endNum; $i++) {
+    echo $i.' ';
+    $sum += $i;
+}
+echo PHP_EOL;
+echo 'Sum: '.$sum;
---
+++ PHP_Fundamentals/Syntax,_Conditional_Loops/Lab/10. Multiplication Table.php
@@ -0,0 +1,9 @@
+<?php
+$num = intval(readline());
+
+for($i = 1; $i<=10; $i++) {
+    echo "$num X $i = ".($num*$i).PHP_EOL;
+
+}
+
+
