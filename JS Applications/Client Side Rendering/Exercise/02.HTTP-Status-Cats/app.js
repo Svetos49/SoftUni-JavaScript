d694b17317febe23519a83974dd990278ee1214c
Svetoslav Slavov <75615836+Svetos49@users.noreply.github.com>
2023-06-02 11:35:37 +0300

    Add files via upload

---
+++ JS Applications/Client Side Rendering/Exercise/02.HTTP-Status-Cats/app.js
@@ -0,0 +1,38 @@
+import {html, render} from '../node_modules/lit-html/lit-html.js';
+import {cats as catData} from './catSeeder.js';
+
+// on application start
+// import cat array
+// map cat array to cat cards
+// render result
+
+const catCardTemplate = (cat) => html`
+<li>
+<img src="./images/${cat.imageLocation}.jpg" width="250" height="250" alt="Card image cap">
+<div class="info">
+    <button class="showBtn">Show status code</button>
+    <div class="status" style="display: none" id=${cat.id}>
+        <h4>Status Code: ${cat.statusCode}</h4>
+        <p>${cat.statusMessage}</p>
+    </div>
+</div>
+</li>`;
+
+const main = document.getElementById('allCats');
+
+// const cats = catData.map(catCardTemplate);
+const catsList = html`
+<ul @click=${toggleInfo}>
+    ${catData.map(catCardTemplate)}
+</ul>`
+render(catsList , main);
+
+function toggleInfo(event) {
+  const element = event.target.parentNode.querySelector('.status');
+
+  if(element.style.display == 'none') {
+      element.removeAttribute('style');
+  } else {
+      element.style.display = 'none';
+  }
+}
