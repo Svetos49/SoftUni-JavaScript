476520ac1d26db0c959ac9d3b531e1ffe02691d2
Svetoslav Slavov <75615836+Svetos49@users.noreply.github.com>
2023-06-21 16:16:16 +0300

    Add files via upload

---
+++ JS Applications/My Homework/06.Blog/app.js
@@ -0,0 +1,73 @@
+function attachEvents() {
+    document.getElementById('btnLoadPosts').addEventListener('click', getPost);
+    document.getElementById('btnViewPost').addEventListener('click', displayPost);
+   
+}
+
+attachEvents();
+async function getPost() {
+    const url = 'http://localhost:3030/jsonstore/blog/posts';
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    const select = document.getElementById('posts');
+    select.innerHTML = '';
+
+   Object.values(data).map(createOption).forEach(o => select.appendChild(o));
+
+  
+}
+function createOption(post) {
+    const result = document.createElement('option');
+    result.textContent = post.title;
+    result.value = post.id;
+
+    return result;
+}
+
+function displayPost() {
+ 
+   const postId = document.getElementById('posts').value;
+  
+  getComment(postId);
+  
+}
+
+async function getComment(postId) {
+  const commentsUl = document.getElementById('post-comments');
+  commentsUl.innerHTML = '';
+
+   const postUrl = 'http://localhost:3030/jsonstore/blog/posts/' + postId;
+   const commentsUrl = 'http://localhost:3030/jsonstore/blog/comments';
+
+ const [postResponce, commentsResponse] = await Promise.all([
+       fetch(postUrl),
+       fetch(commentsUrl)
+   ])
+  
+  
+   const postData = await postResponce.json();
+
+   document.getElementById('post-title').textContent = postData.title;
+   document.getElementById('post-body').textContent = postData.body;
+
+   
+
+   const commentsData = await commentsResponse.json();
+
+   const comments = Object.values(commentsData).filter(c => c.postId == postId);
+
+  
+
+   comments.map(createComment).forEach(c => commentsUl.appendChild(c)); 
+
+  
+}
+
+function createComment(comment) {
+    const result = document.createElement('li');
+    result.textContent = comment.text;
+    result.id = comment.id;
+    return result;
+}
