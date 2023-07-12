a3b952041eb2ea3d93548e7251b76d66c4a9bfd3
Svetoslav Slavov <75615836+Svetos49@users.noreply.github.com>
2023-07-12 09:55:31 +0300

    Add files via upload

---
+++ JS Applications/Exam Preparation/Exam2/src/views/edit.js
@@ -0,0 +1,52 @@
+import { html } from "../../node_modules/lit-html/lit-html.js";
+
+const editTemplate = () => html`
+ <section id="edit">
+          <div class="form">
+            <h2>Edit item</h2>
+            <form class="edit-form">
+              <input
+                type="text"
+                name="brand"
+                id="shoe-brand"
+                placeholder="Brand"
+              />
+              <input
+                type="text"
+                name="model"
+                id="shoe-model"
+                placeholder="Model"
+              />
+              <input
+                type="text"
+                name="imageUrl"
+                id="shoe-img"
+                placeholder="Image url"
+              />
+              <input
+                type="text"
+                name="release"
+                id="shoe-release"
+                placeholder="Release date"
+              />
+              <input
+                type="text"
+                name="designer"
+                id="shoe-designer"
+                placeholder="Designer"
+              />
+              <input
+                type="text"
+                name="value"
+                id="shoe-value"
+                placeholder="Value"
+              />
+
+              <button type="submit">post</button>
+            </form>
+          </div>
+        </section>`;
+
+export function editPage(ctx) {
+    ctx.render(editTemplate());
+}
