b321fda21e4d1e6de11734b1aa9cd21ba02ab6b5
Svetoslav Slavov <75615836+Svetos49@users.noreply.github.com>
2023-07-22 13:48:41 +0300

    Add files via upload

---
+++ JS Applications/Exam Preparation/GamesPlay03/src/views/catalog.js
@@ -0,0 +1,39 @@
+import { html } from "../../node_modules/lit-html/lit-html.js";
+
+
+import * as GameService from '../api/games.js';
+
+const catalogTemplate = (games) => html`
+  <section id="catalog-page">
+            <h1>All Games</h1>
+         
+        ${games.length > 0
+        ? games.map(cardTemplate)
+        : html` <h3 class="no-articles">No articles yet</h3>` }  
+        
+       
+    </section>
+`;
+
+// let gamsList = (games) => {
+//     if(games.length == 0) {
+//         return  html` <h3 class="no-articles">No articles yet</h3>`;
+//     }  else {
+//         return games.map(cardTemplate);
+//     }
+// }
+
+const cardTemplate = (game) => html`
+<div class="allGames">
+<div class="allGames-info">
+    <img src=${game.imageUrl}>
+    <h6>${game.category}</h6>
+    <h2>${game.title}</h2>
+    <a href="/details/${game._id}" class="details-button">Details</a>
+</div>
+</div>`;
+
+export async function catalogPage(ctx) {
+    const games = await GameService.getAll(); 
+    ctx.render(catalogTemplate(games));
+}
