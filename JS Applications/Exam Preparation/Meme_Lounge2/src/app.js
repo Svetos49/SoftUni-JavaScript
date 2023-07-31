42db45b5dd526c0cb11aec62ac92b0d7a650f353
Svetoslav Slavov <75615836+Svetos49@users.noreply.github.com>
2023-07-31 11:45:47 +0300

    Add files via upload

---
+++ JS Applications/Exam Preparation/Meme_Lounge2/src/app.js
@@ -0,0 +1,31 @@
+import {render} from '../node_modules/lit-html/lit-html.js';
+import page from  '../node_modules/page/page.mjs';
+
+
+import {homePage} from './views/home.js';
+
+const main = document.querySelector('main');
+setUserNav();
+
+page('/', decorateContext, homePage);
+
+
+page.start();
+
+function decorateContext(ctx, next) {
+    ctx.render = (content) => render(content, main);
+    ctx.setUserNav = setUserNav;
+
+    next();
+}
+
+function setUserNav() {
+    const token = sessionStorage.getItem('authToken');
+    if(token != null) {
+        document.querySelector('.user').style.display = ''; 
+        document.querySelector('.guest').style.display = 'none';
+    } else {
+        document.querySelector('.user').style.display = 'none'; 
+        document.querySelector('.guest').style.display = '';
+    }
+}
