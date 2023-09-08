3b43c613a51e3162f50e97501bfe30922426a5b8
Svetoslav Slavov <75615836+Svetos49@users.noreply.github.com>
2023-09-08 10:21:44 +0300

    Add files via upload

---
+++ JS Applications/Ohters homeworks/Homework6.1/01.Furniture/src/app.js
@@ -0,0 +1,17 @@
+import { page } from './lib.js';
+import { catalogPage } from './views/catalog.js';
+import { createPage } from './views/create.js';
+import { detailsPage } from './views/details.js';
+import { editPage } from './views/edit.js';
+import { loginPage } from './views/login.js';
+import { registerPage } from './views/register.js';
+
+page('/', catalogPage);
+page('/details/:id', detailsPage);
+page('/create', createPage);
+page('/edit/:id', editPage);
+page('/login', loginPage);
+page('/register', registerPage);
+page('/my-furniture', catalogPage);
+
+page.start();
