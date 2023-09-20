926f30766623a84b48700766f716579daa6d8f66
Svetoslav Slavov <75615836+Svetos49@users.noreply.github.com>
2023-09-20 13:55:34 +0300

    Update metricConverter.php

--- Programming Basics with PHP/metricConverter.php
+++ Programming Basics with PHP/metricConverter.php
@@ -1,15 +1,1 @@
-<?php
-$speed = floatval(readline());
 
-
-if($speed <= 10) {
-    echo "slow";
-} else if($speed <= 50){
-    echo "average";
-} else if($speed <=150) {
-    echo "fast";
-} else if($speed <= 1000) {
-    echo "ultra fast";
-} else {
-    echo "extremely fast";
-}
